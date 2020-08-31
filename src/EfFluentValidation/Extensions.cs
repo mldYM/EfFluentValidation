3d755abfe78d1f90949f8dd219f05fadb5d04206
Simon Cropp <simon.cropp@gmail.com>
2020-08-31 14:27:31 +1000

    Update Extensions.cs

--- src/EfFluentValidation/Extensions.cs
+++ src/EfFluentValidation/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
@@ -11,7 +12,45 @@ static class Extensions
         tracker.DetectChanges();
         return tracker
             .Entries()
-            .Where(x=>x.State == EntityState.Added ||
-                      x.State == EntityState.Modified);
+            .Where(x => x.State == EntityState.Added ||
+                        x.State == EntityState.Modified);
+    }
+
+    public static async Task<List<T>> ToAsyncList<T>(this IAsyncEnumerable<T> enumerable)
+    {
+        var list = new List<T>();
+        await foreach (var item in enumerable)
+        {
+            list.Add(item);
+        }
+
+        return list;
+    }
+
+    public static IEnumerable<string> ChangedProperties(this EntityEntry entry)
+    {
+        return entry.Properties
+            .Where(x =>
+            {
+                if (!x.IsModified)
+                {
+                    return false;
+                }
+
+                var original = x.OriginalValue;
+                var current = x.CurrentValue;
+                if (ReferenceEquals(original, current))
+                {
+                    return false;
+                }
+
+                if (original == null)
+                {
+                    return true;
+                }
+
+                return !original.Equals(current);
+            })
+            .Select(x=>x.Metadata.Name);
     }
 }
